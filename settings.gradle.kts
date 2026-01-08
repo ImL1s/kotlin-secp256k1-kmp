c05697f6589e9762b470cf01824d3ad56d961806
yami <Yami-RND@skvip.org>
2026-01-08 10:53:37 +0800

    Fix CI: Resolve Android plugin resolution conflict

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,8 @@ pluginManagement {
     }
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
-                useModule("com.android.tools.build:gradle:8.13.1")
+            if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
+                useVersion("2.1.0")
             }
         }
     }
